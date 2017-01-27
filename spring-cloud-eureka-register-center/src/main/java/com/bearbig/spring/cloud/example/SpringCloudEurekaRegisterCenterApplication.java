a51b9e6676fe941f1e380218518479204dcab720
熊宇 <xiongyu@xiongyudeMacBook-Pro.local>
2017-01-27 09:21:11 +0800

    init

---
+++ spring-cloud-eureka-register-center/src/main/java/com/bearbig/spring/cloud/example/SpringCloudEurekaRegisterCenterApplication.java
@@ -0,0 +1,12 @@
+package com.bearbig.spring.cloud.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaRegisterCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaRegisterCenterApplication.class, args);
+	}
+}
