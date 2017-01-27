4e13f61c5994dd1158144dd18a962a4f5dbafcf1
BearBig9-9 <BearBig9_9@163.com>
2017-01-27 09:59:18 +0800

    配置启用注册中心

--- spring-cloud-eureka-register-center/src/main/java/com/bearbig/spring/cloud/example/SpringCloudEurekaRegisterCenterApplication.java
+++ spring-cloud-eureka-register-center/src/main/java/com/bearbig/spring/cloud/example/SpringCloudEurekaRegisterCenterApplication.java
@@ -2,7 +2,9 @@ package com.bearbig.spring.cloud.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class SpringCloudEurekaRegisterCenterApplication {
 
